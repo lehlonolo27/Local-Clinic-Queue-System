074a0416bf2870faaff05e24b6190ec63e7b770b
Lehlonolo Mokoena <126181969+lehlonolo27@users.noreply.github.com>
2025-05-05 15:49:39 +0200

    Added Patient Class

---
+++ ClinicQueueSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ClinicQueueSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ ClinicQueueSystem/Models/Patient.cs
@@ -0,0 +1,13 @@
+namespace ClinicQueueSystem.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public bool IsEmergency { get; set; }
+        public DateTime RegistrationTime { get; set; }
+        public int QueueNumber { get; set; }
+        public bool IsServed { get; set; }
+    }
+}
